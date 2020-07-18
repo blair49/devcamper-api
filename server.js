b550ba5527cc87c0dd2ce4c9ee02e89830c7d3a5
blair49 <31095601+blair49@users.noreply.github.com>
2020-07-18 15:49:26 +0530

    Created router for courses

--- server.js
+++ server.js
@@ -11,16 +11,21 @@ connectDB();
 
 //route files
 const bootcamps = require('./routes/bootcamps');
+const courses = require('./routes/courses');
 
 const app = experess();
 
+//Body parser
 app.use(experess.json());
 
+//Dev logging middleware
 if(process.env.NODE_ENV === 'development'){
     app.use(morgan('dev'));
 }
 
+//Mount routers
 app.use('/api/v1/bootcamps', bootcamps);
+app.use('/api/v1/courses', courses);
 app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;

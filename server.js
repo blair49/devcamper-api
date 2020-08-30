d40357893c8b6017054a57a226ab1383d53997d0
blair49 <31095601+blair49@users.noreply.github.com>
2020-08-30 13:20:48 +0530

    User registration and encrypting password

--- server.js
+++ server.js
@@ -13,7 +13,10 @@ connectDB();
 //route files
 const bootcamps = require('./routes/bootcamps');
 const courses = require('./routes/courses');
+const auth = require('./routes/auth');
+
 const fileUpload = require('express-fileupload');
+
 
 const app = express();
 
@@ -34,6 +37,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 //Mount routers
 app.use('/api/v1/bootcamps', bootcamps);
 app.use('/api/v1/courses', courses);
+app.use('/api/v1/auth', auth);
 app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;

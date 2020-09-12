0dab1a545914ad1796217c0b494045fca7cea43a
blair49 <31095601+blair49@users.noreply.github.com>
2020-09-12 13:04:19 +0530

    Implemented JWT token and Login

--- models/User.js
+++ models/User.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const UserSchema = new mongoose.Schema({
     name: {
@@ -40,5 +41,15 @@ UserSchema.pre('save', async function(next){
     const salt = await bcrypt.genSalt(10);
     this.password = await bcrypt.hash(this.password, salt);
 });
+
+//Sign JWT token and return
+UserSchema.methods.getSignedJwtToken = function(){
+    return jwt.sign({id: this._id}, process.env.JWT_SECRET, { expiresIn: process.env.JWT_EXPIRE});
+}
+
+//Match user entered password with hashed password in database
+UserSchema.methods.matchPassword = async function(inputPassword){
+    return await bcrypt.compare(inputPassword, this.password);
+}
 
 module.exports = mongoose.model('User', UserSchema);
